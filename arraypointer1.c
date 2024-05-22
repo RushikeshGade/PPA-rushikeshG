aa52c57b9694135e0399553914d175c805b625b2
RushikeshGade <155994165+RushikeshGade@users.noreply.github.com>
2024-05-22 13:32:16 +0530

    Add files via upload

---
+++ arraypointer1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[5] = {10,20,30,40,50};
+
+    printf("base adress of array is :%d\n",arr);
+    printf("base adress of array is :%d\n",&arr);
+    printf("base adress of array is :%d\n",&(arr[0]));
+
+
+    printf("first element is :%d\n",arr[0]);
+    printf("second element is :%d\n",arr[1]);
+    printf("third element is :%d\n",2[arr]);
+
+    printf("%d\n",arr[2]);
+    printf("%d\n",*(arr+2));
+    printf("%d\n",*(2+arr));
+    printf("%d\n",arr[2]);
+
+    return 0;
+}
